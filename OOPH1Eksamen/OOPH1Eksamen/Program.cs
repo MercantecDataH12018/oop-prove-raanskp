4b9a0d5d44a691ce7e418090cc031536864d642d
René <raan.skp@edu.mercantec.dk>
2018-11-15 12:43:41 +0100

    Finished all the assignments.

--- OOPH1Eksamen/OOPH1Eksamen/Program.cs
+++ OOPH1Eksamen/OOPH1Eksamen/Program.cs
@@ -14,8 +14,58 @@ namespace OOPH1Eksamen
 
     class Program {
         public static void Main() {
-            //Denne funktion kan du bruge til at teste din kode
-            Console.WriteLine("Hello World!");
+            // Test of age
+            B b = new B();
+            b.age = 5;
+            Console.WriteLine("Age is " + b.age);
+
+            // Test of brand
+            E e = new E();
+            e.brand = "iMac";
+            Console.WriteLine(e.brand);
+
+            // Test of increment a
+            C c = new C();
+            c.a = 5;
+            Console.WriteLine("c is " + c.a);
+            c.IncrementA();
+            Console.WriteLine("c is " + c.a);
+            c.IncrementA();
+            Console.WriteLine("c is " + c.a);
+
+            // Test of getA()
+            G g = new G();
+            Console.WriteLine(g.getA());
+
+            // Test of adding number to numbers
+            F f = new F();
+            f.Insert(12345);
+
+            // Tets of encapsulation
+            D d = new D(1, "abc");
+            Console.WriteLine("A is: " + d.GetA() + " and B is: " + d.GetB());
+
+            // Test of constructor
+            A a = new A(1, "hey", 0.5);
+            Console.WriteLine("a is {0} and b is {1} and c is {2}", a.a, a.b, a.c);
+
+            // Test of A reference in H
+            H h = new H(a);
+            Console.WriteLine("h.a.b is: " + h.a.b);
+
+            // Test of printM
+            I i = new I();
+            i.PrintM();
+
+            // Test of null reference when constructing object
+            J j = new J(null);
+            j.Invoke();
+
+            MyClass mc = new MyClass();
+            mc.myString = "Heyo";
+            mc.a = 22;
+
+            Console.WriteLine("myString is: " + mc.myString + " and mc.a " + mc.a);
         }
     }
 
@@ -24,24 +74,35 @@ namespace OOPH1Eksamen
     {
         public string name = "";
         public int legs = 2;
+        public int age;
     }
 
     //OPG Tilføj en string property kaldet 'brand' til følgende class
     class E
     {
-
+        public string brand;
     }
 
     //OPG Tilføj en metode der kan forstørre 'a' med 1 hver gang den kaldes
     class C
     {
         public int a;
+
+        public void IncrementA()
+        {
+            a += 1;
+        }
     }
 
     //OPG Tilføj en metode kaldet "getA" der kan returnere værdien i a
     class G
     {
         string a = "Hello!";
+
+        public string getA()
+        {
+            return a;
+        }
     }
 
     //OPG Tilføj en metode der kan tilføje et tal til numbers. Numbers skal
@@ -49,14 +110,35 @@ namespace OOPH1Eksamen
     class F
     {
         List<int> numbers = new List<int>();
+
+        public void Insert(int number)
+        {
+            numbers.Add(number);
+        }
     }
 
     //OPG Implementer encapsulation i denne klasse, men gør således det stadig 
     //er muligt at læse værdierne 'a' og 'b'
     class D
     {
-        public int a;
-        public string b;
+        private int a;
+        private string b;
+
+        public D(int a, string b)
+        {
+            this.a = a;
+            this.b = b;
+        }
+
+        public int GetA()
+        {
+            return a;
+        }
+
+        public string GetB()
+        {
+            return b;
+        }
     }
 
     //OPG Lav en constructor der sætter alle properties af følgende klasse
@@ -65,13 +147,25 @@ namespace OOPH1Eksamen
         public int a;
         public string b;
         public double c;
+
+        public A(int a, string b, double c)
+        {
+            this.a = a;
+            this.b = b;
+            this.c = c;
+        }
     }
 
     //OPG klassen H skal have en reference til klasse A, kaldet 'a'. Lav en constructor der
     //sætter denne til reference til et objekt af A
     class H
     {
-        
+        public A a;
+
+        public H(A a)
+        {
+            this.a = a;
+        }
     }
 
     //OPG Ændre funktionen PrintM, således at den ikke crasher programmet hvis den køres.
@@ -80,7 +174,10 @@ namespace OOPH1Eksamen
         public string text;
 
         public void PrintM(){
-            Console.WriteLine("Text is " + text.Length + "long");
+            if (text != null)
+            {
+                Console.WriteLine("Text is " + text.Length + "long");
+            }
         }
     }
 
@@ -91,7 +188,10 @@ namespace OOPH1Eksamen
         public I i;
 
         public void Invoke() {
-            i.PrintM();
+            if (i != null)
+            {
+                i.PrintM();
+            }
         }
 
         public J(I i) {
@@ -102,8 +202,8 @@ namespace OOPH1Eksamen
     //OPG Herunder skal du selv implementere en class kaldet MyClass der
     //er subclass til C. Tilføj et public string felt kaldet 'myString'.
 
-    class MyClass {
-        
+    class MyClass : C {
+        public string myString;
     }
 
 
